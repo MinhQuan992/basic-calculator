d14f7d6c2ea9206da7abfe0e712f3d21af6ec9cb
Trần Minh Quân Võ <vtmquan0909@gmail.com>
2023-02-12 16:25:25 +0700

    initialize the codebase

---
+++ basic-calculator/basic-calculator.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+using namespace std;
+
+void printMenu();
+void calculateFactorial();
+void calculateSum();
+
+int main() {
+	printf("Hello World!");
+}
+
+void printMenu() {
+	
+}
+
+void calculateFactorial() {
+	
+}
+
+void calculateSum() {
+	
+}
